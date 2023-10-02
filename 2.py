30fa803713b5851fe2ad363d43d261bd82ede5c0
plinphon <greatez12345@gmail.com>
2023-10-02 12:12:50 +0700

    frf

--- 2.py
+++ 2.py
@@ -18,6 +18,26 @@ def send_email(msg, email):
 
 # Solution goes here
 
+class Patient:
+    def __init__(self,name,family_name,sex,birthday,email,tel,tel2) :
+        self.name = name
+        self.family_name = family_name
+        self.sex = sex
+        self.birthday = birthday
+        self.email = email
+        self.tel = tel
+        self.tel2 = tel2
+    
+    def greeting(self):
+        return f'Hello {self.sex} {self.name} {self.family_name} '
+        
+
+    def send_appointment_reminder(self,app_time):
+        age = app_time.year - self.birthday.year 
+        print('age: '+str(age)+" years")
+        send_email(self.tel,self.email)
+        send_sms(self.tel,self.tel2)
+
 patient1 = Patient(
     'John',
     'Doe',
